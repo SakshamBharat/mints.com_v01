e4eb9ff8e95a9c8045fbdedb4edd2c3cc499ac08
sakshambharat <sakshamch.chaurasia@gmail.com>
2026-08-11 00:50:32 +0530

    Updated project

--- deepLinkRouter.js
+++ deepLinkRouter.js
@@ -119,5 +119,6 @@ router.get("/reels/:id", async (req, res) => {
         return res.status(500).send("Server error");
     }
 });
+app.get("/.well-known/assetlinks.json", (req, res) => { res.setHeader("Content-Type", "application/json"); res.status(200).send([ { relation: [ "delegate_permission/common.handle_all_urls" ], target: { namespace: "android_app", package_name: "com.sakshambharat.mints.mints", sha256_cert_fingerprints: [ "DE:20:99:78:5A:7B:70:BD:F5:81:7A:E6:9C:23:80:74:42:0E:F4:D6:CA:6E:1A:27:C6:1E:A6:1D:0E:8F:BE:D7" ] } } ]); });
 
 module.exports = router;
